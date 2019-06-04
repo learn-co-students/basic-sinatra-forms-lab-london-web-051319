b9e09598f1b8192e7056811e3a345f21d694a5d5
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-06-04 10:46:19 +0100

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team = "Team Name: #{params["name"]}
+             Coach: #{params["coach"]}
+             Point Guard: #{params["pg"]}
+             Power Forward: #{params["pf"]}
+             Shooting Guard: #{params["sg"]}
+             Center: #{params["c"]}
+            "
+    erb :team
+  end
 
 end
